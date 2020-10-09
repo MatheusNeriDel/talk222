a44de3afdcc9e62f14272ae2bdacb0c08445955a
MatheusNeriDel <matheus.neri.del@gmail.com>
2020-10-09 00:42:26 -0400

    validacao completa do formulario de cadastro e padronizacao de identacao

--- script.js
+++ script.js
@@ -1,31 +1,68 @@
 /*  funções de formulário  */
+function validarEmail(email) {
+  var usuario = email.substring(0, email.indexOf("@"));
+  var dominio = email.substring(email.indexOf("@") + 1, email.length);
 
-function alertarSenhaInvalida()
-{
-	document.forms['cadastro']['senha'].setCustomValidity('Senha muito pequena!');
+  var valido = true;
+
+  if (usuario.length < 1 || dominio.length < 3) valido = false;
+  else if (
+    usuario.indexOf("@") != -1 ||
+    usuario.indexOf(" ") != -1 ||
+    dominio.indexOf("@") != -1 ||
+    dominio.indexOf(" ") != -1 ||
+    dominio.indexOf(".") == -1 ||
+    dominio.indexOf(".") == 0 ||
+    dominio.lastIndexOf(".") == dominio.length - 1
+  )
+    valido = false;
+
+  return valido;
 }
 
-function alertarSenhaDiferente()
-{
-	document.forms['cadastro']['confirmasenha'].setCustomValidity('As senhas devem ser iguais');
+function validarSenha(senha) {
+  if (senha.length < 8) return false;
+
+  return true;
 }
 
-function validarFormulario()
-{
-	var senha = document.forms['cadastro']['senha'].value;
-	var confirmasenha = document.forms['cadastro']['confirmasenha'].value;
+function validarChecagemSenha(senha, confirmasenha) {
+  if (senha != confirmasenha) return false;
 
-	if(senha.length < 8)
-	{
-		alertarSenhaInvalida();
-		return false;
-	}
-
-	if(senha != confirmasenha)
-	{
-		alertarSenhaDiferente();
-		return false;
-	}
-
-	return true;
+  return true;
 }
+
+function mostrarMensagem(id, mensagem) {
+  document.getElementById(id).innerHTML = mensagem;
+}
+
+function limparMensagens() {
+  mensagens = document.getElementsByClassName("msg");
+  for (const e of mensagens) e.innerHTML = "";
+}
+
+/* validação do formulário de cadastro */
+function validarFormCadastro() {
+  var email = document.forms["cadastro"]["email"].value;
+  var senha = document.forms["cadastro"]["senha"].value;
+  var confirmasenha = document.forms["cadastro"]["confirmasenha"].value;
+
+  limparMensagens();
+
+  if (!validarEmail(email)) {
+    mostrarMensagem("msgemail", "O e-mail não é válido");
+    return false;
+  } else if (!validarSenha(senha)) {
+    mostrarMensagem("msgsenha", "A senha deve ter pelo menos 8 caracteres");
+    return false;
+  } else if (!validarChecagemSenha(senha, confirmasenha)) {
+    mostrarMensagem("msgconfsenha", "As senhas devem ser iguais");
+    return false;
+  }
+
+  return true;
+}
+
+/* validação do formulário de login */
+
+/* validação do formulário de mensagem */
